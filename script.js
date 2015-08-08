a5a7582fb28e2a4273bbaebac90bd984b0c4a70b
gecko655 <aqwsedrft1234@yahoo.co.jp>
2015-08-08 15:40:45 +0900

    Bug fix of linking of the notification from facebook's top page.

--- script.js
+++ script.js
@@ -2,12 +2,15 @@ console.log("Don't Remove Poke is working!");
 //For normal page load
 handler();
 //For javascript page load (from notification)
-var observer = new MutationObserver(handler).observe($('.fb_content.clearfix').get(0) ,{childList:true});
+new MutationObserver(handler).observe($('.fb_content.clearfix').get(0) ,{childList:true});
 
 function handler(mutations){
-  $('._50z-').addClass("hidden_elem")
-  new MutationObserver(function(mutations){
-      $('._50z-').addClass("hidden_elem");
-  }).observe($('#poke_live_new').get(0) ,{childList:true,subtree:true});
+  var poke_live_new = $('#poke_live_new').get(0);
+  if(typeof poke_live_new!="undefined"){
+    $('._50z-').addClass("hidden_elem");
+    new MutationObserver(function(mutations){
+        $('._50z-').addClass("hidden_elem");
+    }).observe(poke_live_new ,{childList:true,subtree:true});
+  }
 }
 
